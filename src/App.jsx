bdfec89806ca6e4b069f0383da10587ca6daa135
karen9999 <karen.dev999@gmail.com>
2021-06-26 13:51:32 +0300

    add prettier

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+
+
+const App = () => {
+  return (
+    <>
+  <h1>Hello Soccer!!!</h1>
+  </>
+  )
+};
+
+export default hot(App);
