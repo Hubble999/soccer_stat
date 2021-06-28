d446b7fea6f5269a5974bdb53140d00e716d26ee
karen9999 <karen.dev999@gmail.com>
2021-06-28 23:13:54 +0300

    configure store redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import teamsReducer from './slices/teams';
+import regeneratorRuntime from 'regenerator-runtime';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore({
+  reducer: {
+    teamsState: teamsReducer
+  }
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/selectors/index.js
@@ -0,0 +1,1 @@
+export const teamsSelector = (state) => state.teamsState.teams;
