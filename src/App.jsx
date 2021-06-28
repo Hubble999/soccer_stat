56ad55c4e16104e3ad804d7f646f0df78bfccc17
karen9999 <karen.dev999@gmail.com>
2021-06-28 23:18:20 +0300

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
-
+import Header from './components/Header';
+import Teams from './components/Teams';
+import CalendarTeam from './components/CalendarTeam';
 
 const App = () => {
   return (
-    <>
-  <h1>Hello Soccer!!!</h1>
-  </>
-  )
+    <BrowserRouter>
+    <Header/>
+      <h1>Hello Soccer!!!</h1>
+      <Route path="/" component={Teams} exact />
+      <Route path="/search/:keyword" component={Teams} exact />
+      <Route path="/team/:id" component={CalendarTeam} exact />
+    </BrowserRouter>
+  );
 };
 
 export default hot(App);
