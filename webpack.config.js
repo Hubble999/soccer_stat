9a5ac1dc49955336368adf90e540d3a63a304f3a
karen9999 <karen.dev999@gmail.com>
2021-07-05 11:32:48 +0300

    add mode for production in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const mode = process.env.NODE_ENV || 'development';
 module.exports = {
   entry: './src/index.js',
-  mode: 'development',
+  mode,
   module: {
     rules: [
       {
