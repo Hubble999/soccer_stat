d7edb4c6c80037ae9bbea75c1e39584538da0581
karen9999 <karen.dev999@gmail.com>
2021-07-05 11:37:15 +0300

    created 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
 import Header from './components/ui/Header';
 import Teams from './components/Teams';
 import Home from './components/ui/Home';
+import Page404 from './components/ui/Page404';
 import Tournaments from './components/Tournaments';
 import CalendarTeam from './components/CalendarTeam';
 import CalendarTournament from './components/CalendarTournament';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Header />
-      <Route path="/" component={Home} exact />
-      <Route path="/teams" component={Teams} exact />
-      <Route path="/teams/id/:id" component={CalendarTeam} exact />
-      <Route path="/teams/id/:id/period/:dateFrom/:dateTo" component={CalendarTeam} exact />
-      <Route path="/teams/search/:keyword" component={Teams} exact />
-      <Route path="/tournaments" component={Tournaments} exact />
-      <Route path="/tournaments/id/:id" component={CalendarTournament} exact />
-      <Route
-        path="/tournaments/id/:id/period/:dateFrom/:dateTo"
-        component={CalendarTournament}
-        exact
-      />
-      <Route path="/tournaments/search/:keyword" component={Tournaments} exact />
-    </BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/teams" component={Teams} exact />
+        <Route path="/team/id/:id" component={CalendarTeam} exact />
+        <Route path="/team/id/:id/period/:dateFrom/:dateTo" component={CalendarTeam} exact />
+        <Route path="/teams/search/:keyword" component={Teams} exact />
+        <Route path="/tournaments" component={Tournaments} exact />
+        <Route path="/tournament/id/:id" component={CalendarTournament} exact />
+        <Route
+          path="/tournament/id/:id/period/:dateFrom/:dateTo"
+          component={CalendarTournament}
+          exact
+        />
+        <Route path="/tournaments/search/:keyword" component={Tournaments} exact />
+        <Route component={Page404} />
+      </Switch>
+    </Router>
   );
 };
 
